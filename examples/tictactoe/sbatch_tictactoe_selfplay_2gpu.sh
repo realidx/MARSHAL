ac40c399711ca9e4b4aa85dd15af3bab973f8c0f
Li_Daoxin <lidaoxinldx@gmail.com>
2026-07-03 17:24:15 +0800

    check

--- examples/tictactoe/sbatch_tictactoe_selfplay_2gpu.sh
+++ examples/tictactoe/sbatch_tictactoe_selfplay_2gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=marshal-tictactoe-2gpu
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:h100-47:2
+#SBATCH --gres=gpu:h100-96:2
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=0
